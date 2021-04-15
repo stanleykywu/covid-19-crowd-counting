6f29472ca35550f8852a3d7b8a61c973784fdb81
stanleykywu <stanleykywu@gmail.com>
2021-04-15 12:06:26 -0400

    try resnet50

--- evaluate_classification.py
+++ evaluate_classification.py
@@ -22,7 +22,7 @@ plt.ylabel('Accuracy')
 plt.savefig('loss_experiments/vgg16_accuracy_plots')
 
 
-dt = np.load('loss_experiments/resnet50_classification_losses.npy')
+dt = np.load('loss_experiments/resnet_classification_losses.npy')
 (train_losses, train_accuracies, val_losses, val_accuracies) = dt
 x_data = np.arange(700)
 
@@ -32,7 +32,7 @@ plt.plot(x_data, val_losses, label='Validation CrossEntropy Loss')
 plt.legend()
 plt.xlabel('Number of Epochs')
 plt.ylabel('Loss')
-plt.savefig('loss_experiments/resnet50_loss_plots')
+plt.savefig('loss_experiments/resnet_loss_plots')
 
 
 plt.close()
@@ -41,4 +41,4 @@ plt.plot(x_data, val_accuracies, label='Validation Set Accuracy')
 plt.legend()
 plt.xlabel('Number of Epochs')
 plt.ylabel('Accuracy')
-plt.savefig('loss_experiments/resnet50_accuracy_plots')
+plt.savefig('loss_experiments/resnet_accuracy_plots')
