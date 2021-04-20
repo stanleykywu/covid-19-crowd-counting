84815e331ffc4591d26cd82b66a67e240b053306
stanleykywu <stanleykywu@gmail.com>
2021-04-20 18:16:12 -0400

    try

--- models.py
+++ models.py
@@ -34,12 +34,7 @@ class ResNetTransfer(nn.Module):
             nn.ReLU(inplace=True),
             nn.Conv2d(64, 128, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(128, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 512, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, stride=2),
             nn.Conv2d(128, 1, kernel_size=3, padding=1),
             nn.ReLU(inplace=True)
         )
