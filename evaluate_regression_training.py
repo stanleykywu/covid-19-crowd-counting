3cbb5f53fee7883e8d9e9233e5596ac3d7a72fea
stanleykywu <stanleykywu@gmail.com>
2021-04-20 14:10:05 -0400

    resnet50?

--- evaluate_regression_training.py
+++ evaluate_regression_training.py
@@ -2,43 +2,43 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.pyplot as plt
 
-dt = np.load('loss_experiments/vgg16_losses_adaptive.npy')
-(train_losses, train_r2, val_losses, val_r2) = dt
-x_data = np.arange(700)
+# dt = np.load('loss_experiments/vgg16_losses_adaptive.npy')
+# (train_losses, train_r2, val_losses, val_r2) = dt
+# x_data = np.arange(700)
 
+# plt.plot(x_data, train_losses, label='Training MSE Loss')
+# plt.plot(x_data, val_losses, label='Validation MSE Loss')
+# plt.legend()
+# plt.xlabel('Number of Epochs')
+# plt.ylabel('Loss')
+# plt.savefig('loss_experiments/vgg16_regression_loss_plots')
+
+# plt.close()
+# plt.plot(x_data, train_r2, label='Training Set r2')
+# plt.plot(x_data, val_r2, label='Validation Set r2')
+# plt.legend()
+# plt.xlabel('Number of Epochs')
+# plt.ylabel('r2')
+# plt.savefig('loss_experiments/vgg16_regression_r2_plots')
+
+
+dt = np.load('loss_experiments/resnet50denmap/resnet50_adaptive_losses.npy')
+(train_losses, train_r2, val_losses, val_r2) = dt
+x_data = np.arange(len(train_losses))
+
+plt.close()
 plt.plot(x_data, train_losses, label='Training MSE Loss')
 plt.plot(x_data, val_losses, label='Validation MSE Loss')
 plt.legend()
 plt.xlabel('Number of Epochs')
 plt.ylabel('Loss')
-plt.savefig('loss_experiments/vgg16_regression_loss_plots')
-
-plt.close()
-plt.plot(x_data, train_r2, label='Training Set r2')
-plt.plot(x_data, val_r2, label='Validation Set r2')
-plt.legend()
-plt.xlabel('Number of Epochs')
-plt.ylabel('r2')
-plt.savefig('loss_experiments/vgg16_regression_r2_plots')
-
-
-dt = np.load('loss_experiments/resnet18_adaptive_losses.npy')
-(train_losses, train_r2, val_losses, val_r2) = dt
-x_data = np.arange(700)
-
-plt.close()
-plt.plot(x_data, train_losses, label='Training MSE Loss')
-plt.plot(x_data, val_losses, label='Validation MSE Loss')
-plt.legend()
-plt.xlabel('Number of Epochs')
-plt.ylabel('Loss')
-plt.savefig('loss_experiments/resnet18_regression_loss_plots')
+plt.savefig('loss_experiments/resnet50denmap/resnet50_regression_loss_plots')
 
 
 plt.close()
 plt.plot(x_data, train_r2, label='Training Set r2')
 plt.plot(x_data, train_r2, label='Validation Set r2')
 plt.legend()
-plt.xlabel('r2')
-plt.ylabel('Accuracy')
-plt.savefig('loss_experiments/resnet18_regression_r2_plots')
+plt.xlabel('Number of Epochs')
+plt.ylabel('r2')
+plt.savefig('loss_experiments/resnet50denmap/resnet50_regression_r2_plots')
